681ed813b12c12d23e38ea1f4c10a67cb856bd17
Rob Carnell <carnellr@battelle.org>
2012-03-23 00:00:00 +0000

    version 0.7

--- vignettes/Sweave_Command.r
+++
@@ -1,7 +0,0 @@
-sweavePath <- file.path("C:","Documents and Settings","carnellr","My Documents",
-                        "R Packages","lhs","vignettes")
-                        
-Sweave(file.path(sweavePath, "augmentLHS_Example.Stex"),
-       syntax=SweaveSyntaxLatex,
-       output=file.path(sweavePath, "augmentLHS_Example.tex"))
-
